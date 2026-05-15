17f10c23968cbf144dcc5ffe50c6c55c4634f0b5
Rajib1504 <152308020+Rajib1504@users.noreply.github.com>
2026-05-15 17:34:54 +0530

    feat: initialize client application with React, Vite, and Tailwind CSS - Created Home page component - Configured main entry point for React application with routing. - Generated route tree for TanStack Router. - Established root route with navigation and outlet for rendering child routes.

---
+++ client/src/Pages/Home.jsx
@@ -0,0 +1,13 @@
+export default function Home() {
+  return (
+    <div className="p-6 bg-white rounded-lg shadow-sm border border-gray-100">
+      <h3 className="text-3xl font-bold text-dark mb-4">
+        Welcome to Live Polling App!
+      </h3>
+      <p className="text-gray-600">
+        Create real-time polls, share with friends, and see the results
+        instantly.
+      </p>
+    </div>
+  );
+}
---
+++ client/src/routes/__root.jsx
@@ -0,0 +1,23 @@
+/* eslint-disable react-refresh/only-export-components */
+import { createRootRoute, Link, Outlet } from "@tanstack/react-router";
+import { TanStackRouterDevtools } from "@tanstack/router-devtools";
+
+export const Route = createRootRoute({
+  component: () => (
+    <>
+      {/* Global Navigation Bar */}
+      <nav className="p-4 flex gap-4 bg-primary text-white shadow-md">
+        <Link to="/" className="[&.active]:font-bold text-lg">
+          Home
+        </Link>
+      </nav>
+
+      {/* Main Content Scope */}
+      <main className="p-4 max-w-6xl mx-auto">
+        <Outlet />
+      </main>
+
+      <TanStackRouterDevtools position="bottom-right" />
+    </>
+  ),
+});
